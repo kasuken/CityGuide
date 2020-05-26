7e91b310a0904e822908a6c7126fa1af5fac4d2a
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-05-26 13:31:47 +0000

    api and first byte of PWA

---
+++ src/CityGuide/CityGuide.API/Controllers/CitiesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityGuide.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CityGuide.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+
+        private readonly ILogger<CitiesController> _logger;
+
+        public CitiesController(ILogger<CitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<City> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new City
+            {
+                ID = Guid.NewGuid(),
+                Name = $"City {index}",
+                Description = $"A long description for the city",
+                isActive = true,
+                isFeatured = false,
+                PublishedAt = DateTime.Now.AddDays(-365)
+            })
+            .ToArray();
+        }
+
+    }
+}
---
+++ src/CityGuide/CityGuide.Models/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CityGuide.Models
+{
+    public class City
+    {
+
+        public Guid  ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public bool isFeatured { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public bool isActive { get; set; }
+
+    }
+}
